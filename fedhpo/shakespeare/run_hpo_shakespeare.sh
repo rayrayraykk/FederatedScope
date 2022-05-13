f70604cbd392e50430f1da4864dd11c2530f818e
rayrayraykk <18007356109@163.com>
2022-05-13 16:10:45 +0800

    Add scripts and format code.

--- fedhpo/shakespeare/run_hpo_shakespeare.sh
+++ fedhpo/shakespeare/run_hpo_shakespeare.sh
@@ -7,9 +7,9 @@ cd ../..
 
 dataset=shakespeare
 
-out_dir=out_${dataset} 
+out_dir=out_${dataset}
 
-if [ ! -d $out_dir ];then
+if [ ! -d $out_dir ]; then
   mkdir $out_dir
 fi
 
@@ -21,24 +21,18 @@ dps=(0.0 0.5)
 steps=(1 2 3 4)
 batch_sizes=(32 64 128)
 
-for (( l=0; l<${#lrs[@]}; l++ ))
-do
-    for (( w=0; w<${#wds[@]}; w++ ))
-    do
-        for (( d=0; d<${#dps[@]}; d++ ))
-        do
-            for (( s=0; s<${#steps[@]}; s++ ))
-            do
-                for (( b=0; b<${#batch_sizes[@]}; b++ ))
-                do
-                    for k in {1..3}
-                    do
-                        python federatedscope/main.py --cfg fedhpo/${dataset}/${dataset}.yaml device $cudaid optimizer.lr ${lrs[$l]} optimizer.weight_decay ${wds[$w]} model.dropout ${dps[$d]} federate.local_update_steps ${steps[$s]} data.batch_size ${batch_sizes[$b]} federate.sample_client_rate $sample_rate seed $k outdir ${out_dir}_${sample_rate} expname lr${lrs[$l]}_wd${wds[$w]}_dropout${dps[$d]}_step${steps[$s]}_batch${batch_sizes[$b]}_seed${k} >/dev/null 2>&1
-                    done
-                done
-            done
+for ((l = 0; l < ${#lrs[@]}; l++)); do
+  for ((w = 0; w < ${#wds[@]}; w++)); do
+    for ((d = 0; d < ${#dps[@]}; d++)); do
+      for ((s = 0; s < ${#steps[@]}; s++)); do
+        for ((b = 0; b < ${#batch_sizes[@]}; b++)); do
+          for k in {1..3}; do
+            python federatedscope/main.py --cfg fedhpo/${dataset}/${dataset}.yaml device $cudaid optimizer.lr ${lrs[$l]} optimizer.weight_decay ${wds[$w]} model.dropout ${dps[$d]} federate.local_update_steps ${steps[$s]} data.batch_size ${batch_sizes[$b]} federate.sample_client_rate $sample_rate seed $k outdir ${out_dir}_${sample_rate} expname lr${lrs[$l]}_wd${wds[$w]}_dropout${dps[$d]}_step${steps[$s]}_batch${batch_sizes[$b]}_seed${k} >/dev/null 2>&1
+          done
         done
+      done
     done
+  done
 done
 
 echo "HPO ends."

1e543073d3ef2c3a44e7d85dab56619d21ab3b7a
rayrayraykk <18007356109@163.com>
2022-09-05 18:21:35 +0800

    add abc to

--- federatedscope/core/translator/data_translator.py
+++ federatedscope/core/translator/data_translator.py
@@ -1,3 +1,4 @@
+import abc
 from federatedscope.core.auxiliaries.splitter_builder import get_splitter
 from federatedscope.core.auxiliaries.transform_builder import get_transform
 from federatedscope.core.interface.base_data import ClientData, \
@@ -24,12 +25,16 @@ class BaseDataTranslator(StandaloneDataDict):
         self.splitter = get_splitter(global_cfg)
         if package is not None:
             self.transform_funcs = get_transform(global_cfg, package)
-        datadict = self.split_to_client()
+
+        train, val, test = self.split_train_val_test()
+        datadict = self.split_to_client(train, val, test)
         super(BaseDataTranslator, self).__init__(datadict, global_cfg)
 
+    @abc.abstractmethod
     def split_train_val_test(self):
         """
         Split dataset to train, val, test.
+        This method should be overridden.
 
         Returns:
             split_data (List): List of split dataset, [train, val, test]
@@ -44,31 +49,29 @@ class BaseDataTranslator(StandaloneDataDict):
         split_data = random_split(dataset, [train_size, val_size, test_size])
         return split_data
 
-    def split_to_client(self):
+    def split_to_client(self, train, val, test):
         """
         Split dataset to clients.
 
         Returns:
-            data_dict (dict): dict of `ClientData` with client_idx as key.
+            datadict (dict): dict of `ClientData` with client_idx as key.
 
         """
-        train, val, test = self.split_train_val_test()
-
         train_label_distribution = [x[1] for x in train]
         split_train = self.splitter(train, prior=train_label_distribution)
         split_val = self.splitter(val, prior=train_label_distribution)
         split_test = self.splitter(test, prior=train_label_distribution)
 
         # Build data dict
-        data_dict = {}
+        datadict = {}
         for client_id in range(1, self.global_cfg.federate.client_num + 1):
             if self.client_cfgs is not None:
                 client_cfg = self.global_cfg.clone().defrost()
                 client_cfg.merge_from_other_cfg(
                     self.client_cfgs.get(f'client_{client_id}'))
-            data_dict[client_id] = ClientData(self.loader,
-                                              client_cfg,
-                                              train=split_train[client_id - 1],
-                                              val=split_val[client_id - 1],
-                                              test=split_test[client_id - 1])
-        return data_dict
+            datadict[client_id] = ClientData(self.loader,
+                                             client_cfg,
+                                             train=split_train[client_id - 1],
+                                             val=split_val[client_id - 1],
+                                             test=split_test[client_id - 1])
+        return datadict

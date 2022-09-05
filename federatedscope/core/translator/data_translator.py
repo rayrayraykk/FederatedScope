b0f069950cfe562890e691dbe267f070371b8bc9
rayrayraykk <18007356109@163.com>
2022-09-05 17:46:57 +0800

    add base data translator

--- federatedscope/core/translator/data_translator.py
+++ federatedscope/core/translator/data_translator.py
@@ -4,15 +4,62 @@ from federatedscope.core.interface.base_data import ClientData, \
     StandaloneDataDict
 
 
-class DataTranslator(StandaloneDataDict):
-    def __init__(self, dataset, global_cfg, client_cfg=None, package=None):
+class BaseDataTranslator(StandaloneDataDict):
+    def __init__(self,
+                 dataset,
+                 global_cfg,
+                 client_cfgs=None,
+                 loader=None,
+                 package=None):
+        self.dataset = dataset
+        self.loader = loader
+        self.global_cfg = global_cfg.clone()
+        self.client_cfgs = client_cfgs
+
         self.splitter = get_splitter(global_cfg)
-        self.client_cfg = client_cfg
         if package is not None:
             self.transform_funcs = get_transform(global_cfg, package)
-        self.process()
-        super(DataTranslator, self).__init__(dataset, global_cfg)
+        datadict = self.process()
+        super(BaseDataTranslator, self).__init__(datadict, global_cfg)
 
-    @staticmethod
-    def process(self):
-        raise NotImplementedError
+    def split_train_val_test(self):
+        from torch.utils.data.dataset import random_split
+
+        dataset, splits = self.global_cfg.data.splits, self.dataset
+        train_size = int(splits[0] * len(dataset))
+        val_size = int(splits[1] * len(dataset))
+        test_size = len(dataset) - train_size - val_size
+
+        return random_split(dataset, [train_size, val_size, test_size])
+
+    def split_to_client(self):
+        data_dict = {
+            x: {}
+            for x in range(1, self.global_cfg.federate.client_num + 1)
+        }
+        train, val, test = self.split_train_val_test()
+
+        # Build data dict
+        train_label_distribution = [x[1] for x in train]
+        split_train = self.splitter(train, prior=train_label_distribution)
+        split_val = self.splitter(val, prior=train_label_distribution)
+        split_test = self.splitter(test, prior=train_label_distribution)
+
+        data_dict = {}
+        for client_id in range(1, self.global_cfg.federate.client_num + 1):
+            if self.client_cfgs is not None:
+                client_cfg = self.global_cfg.clone().defrost()
+                client_cfg.merge_from_other_cfg(
+                    self.client_cfgs.get(f'client_{client_id}'))
+            data_dict[client_id] = ClientData(self.loader,
+                                              client_cfg,
+                                              train=split_train[client_id - 1],
+                                              val=split_val[client_id - 1],
+                                              test=split_test[client_id - 1])
+
+        data_dict = {
+            x: ClientData(self.loader, )
+            for x in range(1, self.global_cfg.federate.client_num + 1)
+        }
+
+        return data_dict

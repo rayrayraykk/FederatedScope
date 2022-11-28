e86d12ce3737b948186556202356d14c7da5de12
rayrayraykk <18007356109@163.com>
2022-11-28 16:21:39 +0800

    fix minor bugs

--- benchmark/FedHPOBench/scripts/lr/run.sh
+++ benchmark/FedHPOBench/scripts/lr/run.sh
@@ -2,6 +2,11 @@ set -e
 
 cudaid=$1
 dataset=$2
+lr=$3
+seed=$4
+
+# lrs=(0.00001 0.0001 0.001 0.01 0.1 1.0)
+# seed=(1 2 3)
 
 cd ../../../..
 
@@ -14,26 +19,19 @@ fi
 echo "HPO starts..."
 
 sample_rates=(1.0)
-lrs=(0.00001 0.0001 0.001 0.01 0.1 1.0)
 wds=(0.0 0.001 0.01 0.1)
 steps=(1 2 3 4)
 batch_sizes=(8 16 32 64 128 256)
 
 for (( sr=0; sr<${#sample_rates[@]}; sr++ ))
 do
-    for (( l=0; l<${#lrs[@]}; l++ ))
+    for (( w=0; w<${#wds[@]}; w++ ))
     do
-        for (( w=0; w<${#wds[@]}; w++ ))
+        for (( s=0; s<${#steps[@]}; s++ ))
         do
-            for (( s=0; s<${#steps[@]}; s++ ))
+            for (( b=0; b<${#batch_sizes[@]}; b++ ))
             do
-                for (( b=0; b<${#batch_sizes[@]}; b++ ))
-                do
-                    for k in {1..3}
-                    do
-                        python federatedscope/main.py --cfg benchmark/FedHPOBench/scripts/lr/${dataset}@openml.yaml device $cudaid  train.optimizer.lr ${lrs[$l]} train.optimizer.weight_decay ${wds[$w]} train.local_update_steps ${steps[$s]} data.type ${dataset}@openml data.batch_size ${batch_sizes[$b]} federate.sample_client_rate ${sample_rates[$sr]} seed $k outdir lr/${out_dir}_${sample_rates[$sr]} expname lr${lrs[$l]}_wd${wds[$w]}_dropout0_step${steps[$s]}_batch${batch_sizes[$b]}_seed${k} >/dev/null 2>&1
-                    done
-                done
+                python federatedscope/main.py --cfg benchmark/FedHPOBench/scripts/lr/${dataset}@openml.yaml device $cudaid  train.optimizer.lr ${lr} train.optimizer.weight_decay ${wds[$w]} train.local_update_steps ${steps[$s]} data.type ${dataset}@openml data.batch_size ${batch_sizes[$b]} federate.sample_client_rate ${sample_rates[$sr]} seed ${seed} outdir lr/${out_dir}_${sample_rates[$sr]} expname lr${lr}_wd${wds[$w]}_dropout0_step${steps[$s]}_batch${batch_sizes[$b]}_seed${seed} >/dev/null 2>&1
             done
         done
     done
